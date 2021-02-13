aacb0221c3407a89c6eb5ec387503360acd181e8
Yermachekov_Oleksii <ermachenkov83@gmail.com>
2021-02-13 16:01:34 +0200

    5/ создаю БД

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask-test.db'
+db = SQLAlchemy(app)
+
+
+class Comments(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.Text, nullable=False)
+    person = db.Column(db.String(80), nullable=False)
+    person_info = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    isActive = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return '<Comments %r>' % self.id
 
 
 @app.route('/')
