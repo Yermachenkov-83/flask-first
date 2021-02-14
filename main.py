2981425c040fca05e139cabe8677fa7a8224545f
Yermachekov_Oleksii <ermachenkov83@gmail.com>
2021-02-14 21:08:32 +0200

    6/ добавление в базу данных новых комментов через отдельную страницу.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask-test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
@@ -28,6 +29,25 @@ def index():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/create', methods=['POST', 'GET'])
+def create():
+    if request.method == 'POST':
+        comment = request.form['comment']
+        person = request.form['person']
+        person_info = request.form['person_info']
+
+        new = Comments(comment=comment, person=person, person_info=person_info)
+
+        try:
+            db.session.add(new)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error"
+    else:
+        return render_template('create.html')
 
 
 if __name__ == '__main__':
